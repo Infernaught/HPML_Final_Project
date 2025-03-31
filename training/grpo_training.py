4821b139e6fedf4585d88eb9872749b92fe0d04e
Infernaught <89timw@gmail.com>
2025-03-31 17:50:28 -0400

    Update device

--- training/grpo_training.py
+++ training/grpo_training.py
@@ -1,6 +1,10 @@
 from datasets import load_dataset
 from trl import GRPOConfig, GRPOTrainer
+from transformers import AutoModelForCausalLM
+import torch
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}")
 dataset = load_dataset("trl-lib/tldr", split="train")
 model = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 
@@ -9,6 +13,7 @@ def reward_len(completions, **kwargs):
     return [-abs(20 - len(completion)) for completion in completions]
 
 training_args = GRPOConfig(output_dir="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", logging_steps=10)
+model = AutoModelForCausalLM.from_pretrained(model).to(device)
 trainer = GRPOTrainer(
     model=model,
     reward_funcs=reward_len,
