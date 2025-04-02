4a0a81b99370381b9b50e46f2b2fe0ae290a9ddd
Infernaught <89timw@gmail.com>
2025-04-01 22:47:16 -0400

    Update eval_dataset

--- training/grpo_training.py
+++ training/grpo_training.py
@@ -7,6 +7,7 @@ import torch
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
 dataset = load_dataset("trl-lib/tldr", split="train")
+eval_dataset = load_dataset("trl-lib/tldr", split="test")
 model = "Qwen/Qwen2.5-1.5B-Instruct"
 
 # Define the reward function, which rewards completions that are close to 20 characters
@@ -33,6 +34,8 @@ model.print_trainable_parameters()  # Print the percentage of trainable paramete
 training_args = GRPOConfig(
     output_dir="Qwen/Qwen2.5-1.5B-Instruct",
     logging_steps=10,
+    save_strategy="steps",        # Save by steps instead of epochs
+    eval_strategy="steps",
     save_steps=500,              # Save checkpoint every 500 steps
     save_total_limit=3,          # Keep only the last 3 checkpoints
     load_best_model_at_end=True, # Load the best model when training ends
@@ -44,5 +47,6 @@ trainer = GRPOTrainer(
     reward_funcs=reward_len,
     args=training_args,
     train_dataset=dataset,
+    eval_dataset=eval_dataset,
 )
 trainer.train()
