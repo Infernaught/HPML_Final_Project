830033c7bf42107dd5ed525b78ae58272030db34
Troy Daniels <109168010+TroyDanielsProjects@users.noreply.github.com>
2025-04-16 22:24:17 -0400

    Added Memory Tracking. Changed file structure
    Added Memory report statements that can print out memory allocations at points in time (commented out). Added record memory history that will track memory allocation over time and save to pickle at end of execution (commented out). And added profiling so we can see data allocations and time management.

--- training/grpo_training.py
+++ training/grpo_training.py
@@ -1,16 +1,38 @@
+import sys
+import os
+# Add the root directory to sys.path so Python can find 'tasks'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pandas as pd
 from trl import GRPOConfig, GRPOTrainer
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, TrainerCallback
+from torch.profiler import profile, record_function, ProfilerActivity
 from peft import LoraConfig, get_peft_model
 import torch
 from datasets import Dataset
-from constants import BASE_MODEL
-from reward_functions import reward_function_mapping
+from training.constants import BASE_MODEL
+from training.reward_functions import reward_function_mapping
+
+
+# create a function to print out memory allocations
+def gpu_memory_report(print_stmt = ""):
+    # get allocations (current and max seen) (bytes) and convert to Gigabytes
+    allocated_Gbytes = torch.cuda.memory_allocated() * 1e-9
+    max_allocated_Gbytes = torch.cuda.max_memory_allocated() * 1e-9
+    print(f"{print_stmt} GPU Memory Allocated: {allocated_Gbytes:.5f} GBs")
+    print(f"{print_stmt} GPU Memory Max Allocated: {max_allocated_Gbytes:.5f} GBs")
+
+# Want to be able to call this inside the training loop. Can use Huggingface Callback
+class GPUMemoryCallback(TrainerCallback):
+    def on_step_end(self, args, state, control, **kwargs):
+        gpu_memory_report(f"Step {state.global_step} - ")
+
+# torch.cuda.memory._record_memory_history() # will record memory allocation over time (then save to pickle at end)
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using device: {device}")
-dataset = pd.read_json("../tasks/datasets/aime_train_dataset.jsonl", orient="records", lines=True)
-eval_dataset = pd.read_json("../tasks/datasets/aime_eval_dataset.jsonl", orient="records", lines=True)
+dataset = pd.read_json("../tasks/aime/aime_train_dataset.jsonl", orient="records", lines=True)
+eval_dataset = pd.read_json("../tasks/aime/aime_eval_dataset.jsonl", orient="records", lines=True)
 dataset = Dataset.from_pandas(dataset)
 eval_dataset = Dataset.from_pandas(eval_dataset)
 
@@ -34,17 +56,22 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_use_double_quant=True,
 )
 
+# torch.cuda.reset_peak_memory_stats() # reset max GPU Memory allocation (profile = not needed)
+
 # Load the base model with quantization
 base_model = AutoModelForCausalLM.from_pretrained(
     BASE_MODEL,
     quantization_config=bnb_config,
     device_map="auto",
     torch_dtype=torch.float16,
+    attn_implementation="eager", # - use flash_attention_2 for A100/H100
 ).to(device)
-
+gpu_memory_report("After Loading Model -")
 # Apply LoRA to the model
 model = get_peft_model(base_model, lora_config)
 model.print_trainable_parameters()  # Print the percentage of trainable parameters
+
+gpu_memory_report("After LoRA -")
 
 training_args = GRPOConfig(
     output_dir=f"outputs/{BASE_MODEL}",
@@ -70,5 +97,19 @@ trainer = GRPOTrainer(
     args=training_args,
     train_dataset=dataset,
     eval_dataset=eval_dataset,
+    # callbacks=[GPUMemoryCallback] # prints out memory allocation for each step
 )
-trainer.train()
+with profile(
+    activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+    profile_memory=True,
+    record_shapes=True,
+    with_stack=True,
+    with_flops=True,
+    schedule=torch.profiler.schedule(
+        wait=1, warmup=1, active=3, repeat=1  # optional tuning
+    ),
+    on_trace_ready=torch.profiler.tensorboard_trace_handler("logs/profiler")  # <- Saves trace files
+) as prof:
+    with record_function("grpo_training"):
+        trainer.train()
+# torch.cuda.memory._dump_snapshot("outputs/my_snapshot.pickle") # with profileing not needed
