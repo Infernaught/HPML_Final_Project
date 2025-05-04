47546fcb7fdf1cd8370d1d3b36a5fd769c6bb534
Infernaught <89timw@gmail.com>
2025-05-04 19:34:17 -0400

    Update reward_functions

--- training/reward_functions.py
+++ training/reward_functions.py
@@ -1,9 +1,14 @@
 import tasks.aime.reward_functions as aime_reward_functions
+import tasks.countdown.reward_functions as countdown_reward_functions
 
 reward_function_mapping = {
     "aime": [
         aime_reward_functions.format_reward_func,
         aime_reward_functions.returns_int_reward_func,
         aime_reward_functions.equation_reward_func,
+    ],
+    "countdown": [
+        countdown_reward_functions.format_reward_func,
+        countdown_reward_functions.equation_reward_func,
     ]
 }
